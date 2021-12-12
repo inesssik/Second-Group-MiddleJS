4ad47146fa95d2ec7817f130a870651fb0a7aa6c
yungsasha777 <yungsasha777@gmail.com>
2021-12-12 13:46:03 +0300

    FinalEditOfProducts

--- script.js
+++ script.js
@@ -9,11 +9,15 @@ fetch(url).then(response => {
                                         <p class='product_model'>Model: ${data.products[i].productModel}</p>
                                         <p class='product_price'>Price: ${data.products[i].productPrice}</p>
                                         <img class='product_photo' src='${data.products[i].productPhoto}' alt=''>
-                                        <button class='button' onclick='addToCart(i)'>Buy</button>
+                                        <button class='button' onclick='addToCart(${data.products[i].productId})'>Buy</button>
                                     </div>`
     }
 {
     
 }
 })
+
+function addToCart(id){
+    console.log(id)
+}
 
